e8243fd410c86a5ef24b7e31eda42f4482d4f576
Divya Malempati <ddivyad2015@gmail.com>
2022-11-14 21:39:56 +0530

    ghbfe

--- JavaPractice/src/JTest.java
+++ JavaPractice/src/JTest.java
@@ -4,7 +4,7 @@ public class JTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Avinash O");
+		System.out.println("Avinash Okjdfn");
 	}
 
 }
